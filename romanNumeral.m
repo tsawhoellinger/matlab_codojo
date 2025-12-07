4737c9e1a0d2996daaad8c2f8866a7456450894c
Wolfgang Hoellinger TSA <wolfgang.hoellinger@t-systems.com>
2025-12-07 21:32:47 +0100

    feature/romanNumeral (#1)
    * TDD 1 - test & implement "I"s u. "M"s
    
    * TDD 2a - test for error handling
    
    * TDD 2b - implement error handling - TDD passed
    
    * TDD 3 - added 'C' coverage
    
    * TIDY FIRST - refactoring
    
    * complete solution

--- romanNumeral.m
+++ romanNumeral.m
@@ -5,8 +5,25 @@ function roman = romanNumeral(n)
 %   Example:
 %       romanNumeral(1999)   % returns 'MCMXCIX'
 
-    if n == 1
-        roman = 'I';
+    if ~isscalar(n) || n ~= floor(n)
+        error('Input must be an integer')
+    end
+
+    if n < 1 || n > 3999
+        error('romanNumeral:OutOfRange', ...
+              'Input must be in the range 1..3999 (standard Roman numeral limit).');
+    end
+
+
+    values  = [1000 900 500 400 100 90 50 40 10 9 5 4 1];
+    symbols = {'M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'};
+
+    roman = '';
+    for i = 1:numel(values)
+        while n >= values(i)
+            roman = [roman symbols{i}];
+            n = n - values(i);
+        end
     end
 end
 
--- testRomanNumeral.m
+++ testRomanNumeral.m
@@ -6,6 +6,38 @@ classdef testRomanNumeral < matlab.unittest.TestCase
             tc.verifyEqual(romanNumeral(1), 'I');
         end
 
+        function testPositive3(tc)
+            tc.verifyEqual(romanNumeral(3), 'III');
+        end
+
+        function testPositive3000(tc)
+            tc.verifyEqual(romanNumeral(3000), 'MMM');
+        end
+
+        function testPositive300(tc)
+            tc.verifyEqual(romanNumeral(300), 'CCC');
+        end
+
+        function testPositive1964(tc)
+            tc.verifyEqual(romanNumeral(1964), 'MCMLXIV');
+        end
+
+        function testPositive2202(tc)
+            tc.verifyEqual(romanNumeral(2202), 'MMCCII');
+        end
+
+        function testErrorNotAnInteger(tc)
+            tc.verifyError(@() romanNumeral(1.2), '');
+        end
+
+        function testErrorOutOfRange0(tc)
+            tc.verifyError(@() romanNumeral(0), 'romanNumeral:OutOfRange');
+        end
+
+        function testErrorOutOfRange4000(tc)
+            tc.verifyError(@() romanNumeral(4000), 'romanNumeral:OutOfRange');
+        end
+
     end
 end
 
